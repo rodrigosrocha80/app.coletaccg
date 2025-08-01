73a5006fb60ee8d67b83eb50bd864af4cf9140f9
rodrigosrocha80 <rodman80@gmail.com>
2025-08-01 14:57:44 -0300

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Importe BrowserRouter
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Envolva o App com BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
